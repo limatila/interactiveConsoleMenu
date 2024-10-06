3717ca3d5f67cb576c093884e37f04573691ac69
Atila Lima <atilalimade@gmail.com>
2024-10-06 20:42:19 -0300

    Aplicando @exec na Calculadora

--- src/modules/classes/calculadora.py
+++ src/modules/classes/calculadora.py
@@ -1,13 +1,32 @@
+from functools import wraps
+from time import sleep
+def exec(callback): #TODO: ver como importar o @exec de 'outrasFunctions.py'
+    @wraps(callback)
+    def wrapper(*args, **kwargs):
+        print("...executando...")
+        sleep(1)
+        callback(*args, **kwargs)
+    return wrapper
+
+#Classe em si
 class Calculadora:
     def __init__(self):
         self.contaTotal = 0
 
-    def soma(self, num1, num2) -> int:
+    @exec
+    def soma(self, num1: int, num2: int) -> int:
         return num1 + num2
-    def subtracao(self, num1, num2) -> int:
+    @exec
+    def subtracao(self, num1: int, num2: int) -> int:
         return num1 - num2
     
+    @exec
     def conte_1(self) -> None:
         self.contaTotal += 1
 
         print(self.contaTotal) #Display
+
+if __name__ == '__main__':
+    calculadora = Calculadora()
+    calculadora.conte_1()
+    print(calculadora.soma(3, 10))
